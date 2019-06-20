06161eb23c6bcd31e83837bb5c057363b8cfd3de
rawan-1 <rawan.cs18@gmail.com>
2019-06-20 14:56:11 +0300

    Main function Done

--- designs.js
+++ designs.js
@@ -1,10 +1,20 @@
-// Select color input
-// Select size input
+const table = document.getElementById("pixelCanvas");
 
 // When size is submitted by the user, call makeGrid()
+function makeGrid(event) {
+  // Stop the default action of submit
+  event.preventDefault();
 
-function makeGrid() {
+  // get value for width and hight of the Table Canvas
+  const hight = document.getElementById("inputHeight").value;
+  const width = document.getElementById("inputWidth").value;
 
-// Your code goes here!
-
+  for (var i = 0; i < hight; i++) {
+    let row = document.createElement("tr");
+    for (var c = 0; c < width; c++) {
+      let column = document.createElement("td");
+      row.appendChild(column);
+    }
+    table.appendChild(row);
+  }
 }
